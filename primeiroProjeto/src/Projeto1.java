9c6674260d8e50a351fe3254a8ab7eb605d09532
Alexis Puglia <alexis.puglia@gmail.com>
2024-03-20 19:13:22 -0300

    Projeto teste

---
+++ primeiroProjeto/src/Projeto1.java
@@ -0,0 +1,13 @@
+public class Projeto1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Utilizando o if e o else em Java: pequeno teste");
+        int x = 10;
+        int y = 20;
+        if (x > y) {
+            System.out.println("O número " + x + " é maior que " + y);
+        }
+        else {
+            System.out.println("O número " + y + " é maior que " + x);
+        }
+    }
+}
